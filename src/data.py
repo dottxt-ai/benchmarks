0e02ffb90bc174d87268f18eaff4b1eda4c0e00d
Andrew Lapp <andrew@github.rew.la>
2024-10-16 03:59:41 -0400

    Measure runtime and total time

--- src/data.py
+++ src/data.py
@@ -1,3 +1,9 @@
+import json
+from pathlib import Path
+
+SAMPLES_PATH = Path(__file__).parent / "samples"
+
+
 models = [
     "NousResearch/Nous-Hermes-llama-2-7b",  # 32,000 tokens vocabulary
     "gpt2",  # 50,257 tokens vocabulary
@@ -5,39 +11,35 @@ models = [
     "unsloth/gemma-2-2b-it-bnb-4bit",  # 256,128 tokens vocabulary
 ]
 
-regex_cases = [
-    {
-        "name": "Phone Number",
-        "regex": r'\d{3}-\d{2}-\d{4}',
-        "example": '203-22-1234'
-    },
-    {
-        "name": "URL",
-        "regex": r'(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?',
-        "example": 'https://github.com/outlines-dev/outlines'
-    },
-    {
-        "name": "GSM8K",
-        "regex": r'A: [\w \.\*\-=\+,\?/]{10,50}\. The answer is [1-9][0-9]{0,9}\.',
-        "example": 'A: Some thoughts before answering. The answer is 42.'
-    },
-    {
-        "name": "Complex string",
-        "regex": r'(0|[1-9][0-9]*)|true|false|([a-zA-Z_][a-zA-Z_0-9]*)',
-        "example": 'AVeryLongStringtoTest1234'
-    },
-    {
-        "name": "Long integer",
-        "regex": r'\+[1-9]\d{1,14}',
-        "example": '1234567891234'
-    }
-]
 
-json_cases = [
-    {
-        "name": "RPG character",
-        "schema":
-        {
+regex_cases = {
+    "Phone Number": {
+        "regex": r"\d{3}-\d{3}-\d{4}",
+        "samples": json.load(open(SAMPLES_PATH / "phone_number.json")),
+    },
+    "URL": {
+        "regex": r"(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?",
+        "samples": json.load(open(SAMPLES_PATH / "url.json")),
+    },
+    "GSM8K": {
+        "regex": r"A: [\w \.\*\-=\+,\?/]{10,50}\. The answer is [1-9][0-9]{0,9}\.",
+        "samples": json.load(open(SAMPLES_PATH / "gsm8k.json")),
+        # gsm8k.json attribution: https://huggingface.co/datasets/thesven/gsm8k-reasoning
+    },
+    "Complex string": {
+        "regex": r"(0|[1-9][0-9]*)|true|false|([a-zA-Z_][a-zA-Z_0-9]*)",
+        "samples": json.load(open(SAMPLES_PATH / "complex_str.json")),
+    },
+    "Long integer": {
+        "regex": r"\+[1-9]\d{1,14}",
+        "samples": json.load(open(SAMPLES_PATH / "long_integer.json")),
+    },
+}
+
+
+json_cases = {
+    "RPG character": {
+        "schema": {
             "$defs": {
                 "Armor": {
                     "enum": ["leather", "chainmail", "plate"],
@@ -55,10 +57,11 @@ json_cases = [
             "title": "Character",
             "type": "object",
         },
-        "example": """{'name': 'Super Warrior', 'age': 26,  'armor': 'leather', 'armor': 10}""",
+        "samples": list(
+            map(json.dumps, json.load(open(SAMPLES_PATH / "rpg_characters.json")))
+        ),
     },
-    {
-        "name": "Simple nested schema",
+    "Simple nested schema": {
         "schema": {
             "$schema": "http://json-schema.org/draft-04/schema#",
             "title": "Schema for a recording",
@@ -91,6 +94,8 @@ json_cases = [
             },
             "required": ["id", "work", "recording_artists"],
         },
-        "example": """{'id': 999, 'work': {'id': 1, 'name': 'Strasbourg Saint-Denis', 'composer': 'Roy Hargrove'}, 'recording_artists': [{'id': 2, 'name': 'Roy Hargrove', 'functions': ['Trumpet', 'Singing']}]}""",
+        "samples": list(
+            map(json.dumps, json.load(open(SAMPLES_PATH / "recording_schema.json")))
+        ),
     },
-]
+}
